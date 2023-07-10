d0542d6dc3104c0e91c1e29b98a770a67d22996f
OleksandrVaniuta <vaniutasa@gmail.com>
2023-07-10 11:35:02 +0300

    done

--- models/contact.js
+++ models/contact.js
@@ -28,7 +28,9 @@ contactShema.post('seve', handleMongooseError);
 const addSchema = Joi.object({
   name: Joi.string().required(),
   email: Joi.string().email().required(),
-  phone: Joi.string().required(),
+  phone: Joi.string()
+    .pattern(/^\+\d{1,3}\s?\(\d{1,3}\)\s?\d{1,4}-\d{1,4}$/)
+    .required(),
   favorite: Joi.boolean(),
 });
 
