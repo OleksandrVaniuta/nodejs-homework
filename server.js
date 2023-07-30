19b7ea95c4f127b981c16afc7d797b112dcba7de
OleksandrVaniuta <vaniutasa@gmail.com>
2023-07-30 23:34:15 +0300

    dove

--- server.js
+++ server.js
@@ -1,9 +1,7 @@
 const mongoose = require('mongoose');
 const app = require('./app');
 
-const DB_HOST =
-  'mongodb+srv://vaniutasa:G74SZWhzalv6wXhe@cluster0.ft08ttn.mongodb.net/db-contacts?retryWrites=true&w=majority';
-// process.env
+const { DB_HOST } = process.env;
 
 mongoose.set('strictQuery', true);
 
@@ -11,7 +9,7 @@ mongoose
   .connect(DB_HOST)
   .then(() => {
     console.log('Database connection successful');
-     app.listen(3000, () => {
+    app.listen(3000, () => {
       console.log('Server running. Use our API on port: 3000');
     });
   })
